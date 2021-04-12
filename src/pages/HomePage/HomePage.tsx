50188bd85b2516661ce54e22b0cc5f2e5a612b07
Rafal.sz <rafal.sz@tutanota.com>
2021-04-12 21:13:49 +0200

    Update to homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import { Box } from '@material-ui/core';
 import ThreadsList from '../../components/ThreadsList';
+import Categories from 'components/Categories';
+import Forms from 'components/Forms/Forms';
 
 const HomePage: React.FC = () => {
 
     return (
         <>
+            <Forms />
+            <Categories />
             <Box width="50%" m={10}>
                 <ThreadsList threads={[
                     {title: "How to die successfully", author: "rafi", lastReply: "TrytianKurek", replyTime: "10h ago", creationTime: "1 day ago"},
