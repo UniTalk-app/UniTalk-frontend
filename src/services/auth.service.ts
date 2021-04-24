8dc4f8725d4825d5e8c07ce73873106d5e61bbbd
Karolina <k99zielonka@gmail.com>
2021-04-24 17:23:58 +0200

    changed port number to 8080

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/auth/";
+const API_URL = "http://localhost:8080/api/auth/";
 
 class AuthService {
     async login(data: { email: string; password: string }) {
