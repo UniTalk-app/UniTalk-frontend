6ce54c65bdce52db0e2093ace250a06207230c2a
tomekstaszkiewicz <staszkiewicztoma@gmail.com>
2021-05-09 13:39:39 +0200

    Cleanup mainData logic

--- src/pages/HomePage/store/StoreProvider.tsx
+++ src/pages/HomePage/store/StoreProvider.tsx
@@ -1,13 +1,9 @@
 import * as React from "react";
 import MainDataService from "services/mainData.service";
 
-/**
- * We don't want to overcomplicate things for now.
- * I think that we should avoid using reducer, and for the MVP
- * just create a simple structure of getters to receive data from global state.
- */
 const store: MainPageStoreData = {
     getData: MainDataService.getData,
+    // we need to create functions here, because the MainDataService.categories & threads are immutable
     categories: () => MainDataService.categories,
     threads: () => MainDataService.threads,
     subscribeToServiceChange: MainDataService.subscribeToServiceChange
@@ -23,19 +19,30 @@ export const StoreProvider: React.FC = ({children}) => {
     );
 };
 
-// Might wanna improve the naming, useMainData is not so descriptive
 export function useMainData(): MainPageStoreData{
+    /**
+     * 1) Bind to context
+     */
     const ctx = React.useContext<MainPageStoreData>(Context);
-    // We want to force the rerender of components
+    
     const [, forceRerender] = React.useState(true);
     React.useEffect(() => {
+        /**
+         * 2) Call endpoint for main page data
+         */
         ctx.getData();
-        ctx.subscribeToServiceChange(() => forceRerender(p => !p));
-        // we invalidate data every 30 minutes
+        /**
+         * 3) Force rerender after data is invalidated
+         */
+        ctx.subscribeToServiceChange(() => forceRerender(p => !p));  
+        /**
+         * 4) Invalidate data every 30 minutes
+         */
+        const intervalValue = 30 * 60 * 1000;
         const id = setInterval(() => {
             ctx.getData();
             forceRerender(p => !p);   
-        }, 30 * 60 * 1000);
+        }, intervalValue);
 
         return () => {
             clearInterval(id);
