92860457a8a367e7803c41b9e0fcca6ba4c6fb35
jwojc99 <wojciechowskaj.79@gmail.com>
2021-05-20 21:23:20 +0200

    [UNI-152] Test correction

--- src/components/LatestThreads/tests/LatestThreads.test.tsx
+++ src/components/LatestThreads/tests/LatestThreads.test.tsx
@@ -6,15 +6,16 @@ import LatestThreads from "../LatestThreads";
 describe("LatestThreads", () => {
     test("displays table name", async () => {
         const { findByText } = render(<LatestThreads latestthreads={[
-            {name:"Test",comments:"0",}]} />);
+            {title:"Test",threadId:"1",author:"Tester",creationTime:"0",lastReply:"0",replyTime:"0"}]} />);
             
         const result = await findByText("Latest threads");
         const name = await findByText("Test");
-        const comments = await findByText("0 comments");
+       
 
         expect(result).toBeTruthy();
         expect(name).toBeTruthy();
-        expect(comments).toBeTruthy();
+
+
 
 
     });
