3249633f4d1fd1a6b2e01d796685f0ae8d563b93
Paweł <paweltomczyk444@gmail.com>
2021-06-09 13:46:10 +0200

    change dialog content

--- src/components/ThreadsList/EditThreadDialog.tsx
+++ src/components/ThreadsList/EditThreadDialog.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
-import {FormHelperText,DialogActions,Button,TextField,makeStyles, createStyles,DialogContent, Select,} from "@material-ui/core";
+import {Theme,FormControl,InputLabel,MenuItem,FormHelperText,DialogActions,Button,TextField,makeStyles, createStyles,DialogContent, Select,} from "@material-ui/core";
 import threadService from "services/thread.service";
 import { useSnackbar } from "notistack";
 import { Form, Formik } from "formik";
 import storeSubject from "store/store";
 import * as Yup from "yup";
 
-const useStyles = makeStyles(
+const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         combo: {
             width: "100%",
-        }
+        },
+        txtField: {
+            width: "100%",
+            marginBottom: theme.spacing(2),
+        },
     })
 );
 
@@ -50,8 +54,8 @@ const EditThreadForm: React.FC<EditThreadDialogProps> = (props) => {
     return (
         <Formik
             initialValues={{
-                title: "TTTIle new",
-                category:"",
+                title: "",
+                category:"None",
             }}
             validationSchema={validateSchema}
             onSubmit={(values) => {
@@ -64,10 +68,10 @@ const EditThreadForm: React.FC<EditThreadDialogProps> = (props) => {
                     <Form noValidate>
                         <DialogContent>
                             <TextField
+                                className={classes.txtField}
                                 autoComplete="off"
                                 id="title"
-                                label="Title"
-                                rowsMax={1}
+                                label="Name"
                                 color="primary"
                                 variant="outlined"
                                 onChange={props.handleChange}
@@ -77,31 +81,33 @@ const EditThreadForm: React.FC<EditThreadDialogProps> = (props) => {
                                 error={props.touched.title && props.errors.title ? true : false}
                                 helperText={(props.touched.title && props.errors.title) ?? false}
                             />
-                        </DialogContent>
-                        <DialogContent>
-                            <Select
-                                variant="outlined"
-                                className={classes.combo}
-                                id="category"
-                                name="category"
-                                value={props.values.category}
-                                open={openCombo}
-                                onClose={handleSelectClose}
-                                onOpen={handleSelectOpen}
-                                onChange={props.handleChange}
-                                error={props.touched.category && props.errors.category ? true : false}
-                            >
-                                {appData.categories.map(cat => (
-                                    <option  key={cat.categoryId}
-                                        value={cat.categoryId}>
-                                        {cat.name}
-                                    </option >
-                                ))}
+                            <FormControl variant="outlined" className={classes.combo}>
+                                <InputLabel>Categories</InputLabel>
+                                <Select
+                                    id="category"
+                                    name="category"
+                                    value={props.values.category}
+                                    open={openCombo}
+                                    onClose={handleSelectClose}
+                                    onOpen={handleSelectOpen}
+                                    onChange={props.handleChange}
+                                    error={props.touched.category && props.errors.category ? true : false}
+                                >
+                                    <MenuItem  key={-1} value={-1}>
+                                        None
+                                    </MenuItem>
+                                    {appData.categories.map(cat => (
+                                        <MenuItem  key={cat.categoryId}
+                                            value={cat.categoryId}>
+                                            {cat.name}
+                                        </MenuItem >
+                                    ))}
 
-                            </Select>
-                            {props.errors.category && props.touched.category ?
-                                <FormHelperText error={true} >{props.errors.category}</FormHelperText> : null
-                            }
+                                </Select>
+                                {props.errors.category && props.touched.category ?
+                                    <FormHelperText error={true} >{props.errors.category}</FormHelperText> : null
+                                }
+                            </FormControl>
                         </DialogContent>
                         <DialogActions>
                             <Button onClick={handleClose} color="primary">Cancel </Button>
